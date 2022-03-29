c270386050c59498b36f3d32fddfe67b57906fa0
manucaputo <182041@umons.ac.be>
2022-03-29 15:42:21 +0200

    Chap 4 debut video 97

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:test_widgets/models/transaction.dart';
@@ -10,47 +12,67 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: ListView.builder(
-        // if we have long lists ok !! else use juste child: ListView ==> transactions[index] ==> tx
-        // == Column with a SingleChildScrollView
-        itemBuilder: (ctx, index) {
-          return Card(
-              child: Row(children: <Widget>[
-            Container(
-                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                decoration: BoxDecoration(
-                    border: Border.all(
-                  color: Theme.of(context).primaryColor,
-                  width: 2,
-                )),
-                padding: EdgeInsets.all(10),
-                child: Text(
-                  '\$ ${transactions[index].amount.toStringAsFixed(2)}', //max 2 decimals
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Theme.of(context).primaryColor,
-                  ),
-                )),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+      child: transactions.isEmpty
+          ? Column(
               children: <Widget>[
                 Text(
-                  transactions[index].title,
+                  'No transaction added yet !',
                   style: Theme.of(context).textTheme.headline6,
-                  ),
-
-                Text(
-                  DateFormat('dd/MM/yyyy').format(transactions[index].date),
-                  //DateFormat.yMMMd().format(tx.date),
-                  style: TextStyle(color: Colors.grey),
                 ),
+                SizedBox(
+                  height: 10,
+                  ),
+                Container(
+                  height: 200,
+                  child: Image.asset(
+                    'assets/images/Accueil_TITRE.png',
+                    fit: BoxFit.cover,
+                  ),
+                )
               ],
+            )
+          : ListView.builder(
+              // if we have long lists ok !! else use juste child: ListView ==> transactions[index] ==> tx
+              // == Column with a SingleChildScrollView
+              itemBuilder: (ctx, index) {
+                return Card(
+                    child: Row(children: <Widget>[
+                  Container(
+                      margin:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                        color: Theme.of(context).primaryColor,
+                        width: 2,
+                      )),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        '\$ ${transactions[index].amount.toStringAsFixed(2)}', //max 2 decimals
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      )),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        transactions[index].title,
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                      Text(
+                        DateFormat('dd/MM/yyyy')
+                            .format(transactions[index].date),
+                        //DateFormat.yMMMd().format(tx.date),
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                ]));
+              },
+              itemCount: transactions.length,
             ),
-          ]));
-        },
-        itemCount: transactions.length,
-      ),
     );
   }
 }
